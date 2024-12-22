dffd155f91ed840e3976c72e090f6bc69c55550c
luccie-cmd <hoedje201@gmail.com>
2024-12-22 23:35:39 +0100

    Change run.sh to use host CPU

--- script/run.sh
+++ script/run.sh
@@ -4,23 +4,23 @@ qemu-system-x86_64 \
     -drive file="$1/image.img",format=raw,cache=writeback,snapshot=off \
     -M q35 \
     -m 128 \
-    -debugcon stdio \
     -global isa-debugcon.iobase=0xe9 \
+    -debugcon file:debug_output.txt \
     -d cpu_reset,int \
     -no-reboot \
-    # -enable-kvm \
-    # -cpu host
+    -enable-kvm \
+    -cpu host
 elif [ "$2" == "debug" ]; then
 qemu-system-x86_64 \
     -bios /usr/share/OVMF/x64/OVMF.4m.fd \
     -drive file="$1/image.img",format=raw,cache=writeback,snapshot=off \
     -M q35 \
     -m 128 \
-    -debugcon stdio \
     -global isa-debugcon.iobase=0xe9 \
+    -debugcon file:debug_output.txt \
     -d cpu_reset \
     -no-reboot \
     -enable-kvm \
     -cpu host \
-    # -S -s
+    -S -s
 fi
